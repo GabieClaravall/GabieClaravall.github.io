d29e02c2a9d65810f0215530f37e3486233716d9
GabieClaravall <gclaravall18@holynames-sea.org>
2016-08-11 15:43:09 -0700

    My first Website
    Through html, css, and java script

---
+++ web.js
@@ -0,0 +1,20 @@
+       function changeImage(){
+            var img = document.getElementById("Hi");
+            img.src = "cityscroller2.jpg"; 
+        }  
+        
+        function nextImage(){
+            var img = document.getElementById("Hi");
+            img.src = "IMG_4557.jpg"; 
+        }  
+        
+    
+function myFunction() {
+    alert("Hello! I am an alert box!");
+}
+
+function learnaboutme() {
+    alert("Welcome")
+}
+
+    
